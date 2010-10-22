823a657e14859be157f30af56fbd284d60786b06
Skachkov Vlad <skachkov@guns.ru>
2010-10-22 15:16:45 +0300

    initial project structure

---
+++ application/controllers/DriverInformationManagementModuleController.php
@@ -0,0 +1,46 @@
+<?php
+
+class DriverInformationManagementModuleController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function addDriverAction()
+    {
+        // action body
+    }
+
+    public function viewTemporaryDriverAccountsAction()
+    {
+        // action body
+    }
+
+    public function viewTemporaryDriverAccountNotificationAction()
+    {
+        // action body
+    }
+
+    public function viewTemporaryDriverAccountsForHomebaseAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
---
+++ application/controllers/MaintainingDriverQualificationFileController.php
@@ -0,0 +1,74 @@
+<?php
+
+class MaintainingDriverQualificationFileController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function requestNewFormsAction()
+    {
+        // action body
+    }
+
+    public function getHiredOrActiveHomabaseDriversAction()
+    {
+        // action body
+    }
+
+    public function driverInformationAction()
+    {
+        // action body
+    }
+
+    public function driverAddressInputAction()
+    {
+        // action body
+    }
+
+    public function commercialDriverLicenseAndEquipmentOperatedAction()
+    {
+        // action body
+    }
+
+    public function driverEmploymentHistoryAction()
+    {
+        // action body
+    }
+
+    public function incidentsAndViolationsAction()
+    {
+        // action body
+    }
+
+    public function hoursOfServiceAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

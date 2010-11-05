0b8c495cadce037b92ef01ba282a7fca39d4df45
Andryi Ilnytskyi <iliadev@mail.ru>
2010-11-05 15:03:42 +0200

    Implemented equipment form . Fields are grouped in the table with using Zend decorators.
    refs #16

--- application/modules/equipment/models/Equipment.php
+++ application/modules/equipment/models/Equipment.php
@@ -16,4 +16,29 @@ class Equipment_Model_Equipment extends Zend_Db_Table_Abstract
         $select = $equipmentModel->select();
         return $equipmentModel->fetchAll($select);
     }
+
+    /**
+     * @author Andriy Ilnytskyi 05.11.2010
+     *
+     * Create equipment.
+     *
+     * @param array $equipmentRow Array which contains value of the fields
+     * @return mixed
+     */
+    public function createEquipment($equipmentRow)
+    {
+        // create a new row
+        $rowEquipment = $this->createRow();
+        if ($rowEquipment) {
+            foreach ($equipmentRow as $key => $value) {
+                $rowEquipment->$key = $value;
+            }
+
+            $rowEquipment->save();
+            //return the new equipment
+            return $rowEquipment;
+        } else {
+            throw new Zend_Exception("Could not create user!");
+        }
+    }
 }

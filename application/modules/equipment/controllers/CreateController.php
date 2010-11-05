8682cb4207b28d067a4f35b91388f008095227d6
Andryi Ilnytskyi <iliadev@mail.ru>
2010-11-06 00:03:38 +0200

    Impemented storing equipment information. Fixed list view.
    refs #16

--- application/modules/equipment/controllers/CreateController.php
+++ application/modules/equipment/controllers/CreateController.php
@@ -25,7 +25,33 @@ class Equipment_CreateController extends Zend_Controller_Action
             if ($equipmentForm->isValid($_POST)) {
                 $equipmentModel = new Equipment_Model_Equipment();
                 $equipmentRow = array(
- 
+                    //'e_id' => $equipmentForm->getValue(''),
+                    //'e_Number' => $equipmentForm->getValue('VIN'),
+                    //'e_Owner_Number' => $equipmentForm->getValue(''),
+                    'e_Unit_Number' => $equipmentForm->getValue('UnitNumber'),
+//                    'e_Alternate_ID' => $equipmentForm->getValue(''),
+                    'e_RFID_No' => $equipmentForm->getValue('RFID'),
+//                    'e_Entry_Date' => $equipmentForm->getValue(''),
+                    'e_License_Number' => $equipmentForm->getValue('LicPlateNum'),
+                    'e_License_Expiration_Date' => $equipmentForm->getValue('RegExpDate'),
+//                    'e_Start_Mileage' => $equipmentForm->getValue(''),
+                    'e_Registration_State' => $equipmentForm->getValue('State'),
+                    'e_Gross_Vehicle_Weight_Rating' => $equipmentForm->getValue('GVW'),
+                    'e_Gross_Vehicle_Registered_Weight' => $equipmentForm->getValue('GVRW'),
+                    'e_Unladen_Weight' => $equipmentForm->getValue('UnladenWeight'),
+                    'e_Axles' => $equipmentForm->getValue('NumOfAxles'),
+//                    'e_Name' => $equipmentForm->getValue(''),
+                    'e_Year' => $equipmentForm->getValue('Year'),
+                    'e_Make' => $equipmentForm->getValue('Make'),
+                    'e_Color' => $equipmentForm->getValue('Color'),
+                    'e_Model' => $equipmentForm->getValue('Model'),
+//                    'e_Description' => $equipmentForm->getValue(''),
+//                    'e_New_Equipment_Status' => $equipmentForm->getValue(''),
+//                    'e_Active_Status' => $equipmentForm->getValue(''),
+//                    'e_Fee' => $equipmentForm->getValue(''),
+//                    'e_Title_Status' => $equipmentForm->getValue(''),
+//                    'e_Picture' => $equipmentForm->getValue(''),
+//                    'e_DOT_Regulated'=> $equipmentForm->getValue('')
                 );
                 $equipmentModel->createEquipment($equipmentRow);
                 return $this->_redirect('equipment/list'); 

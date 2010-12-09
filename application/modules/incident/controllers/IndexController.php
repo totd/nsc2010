6035c4859f64f2b4a592fc8049dd068a73a4f9a1
Andryi Ilnytskyi <iliadev@mail.ru>
2010-12-09 21:53:12 +0200

    Started an implementation of the incident profile
    refs #44

--- application/modules/incident/controllers/IndexController.php
+++ application/modules/incident/controllers/IndexController.php
@@ -8,7 +8,11 @@ class Incident_IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        
+        $this->view->breadcrumbs = '<a href="/incident/index">Incident Management</a>';
+        $this->view->breadcrumbs .= '&nbsp;&gt;&nbsp;<a href="/incident/list">Incident List</a>';
+        $this->view->breadcrumbs .= '&nbsp;&gt;&nbsp;Incident Profile';
+
+        $this->view->pageTitle = 'INCIDENT INFORMATION WORKSHEET';
     }
 
 }

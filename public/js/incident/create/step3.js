7d15fe61b6184ebca08266b3486bb466652252e9
Andryi Ilnytskyi <iliadev@mail.ru>
2010-12-15 12:13:26 +0200

    Removed step3 in the incident's creation.

--- public/js/incident/create/step3.js
+++ public/js/incident/create/step3.js
@@ -1,9 +1,13 @@
 $(function() {
-    $("#nextSubmit").click(function() {
+    $("#saveSubmit").click(function() {
         document.getElementById("step3Form").submit();
     });
 
     $("#exitSubmit").click(function() {
         window.location.href='/incident/create/exit';
+    });
+
+    $(function(){
+        $(".multiselect").multiselect();
     });
 });
